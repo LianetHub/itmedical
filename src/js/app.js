2ec1d53bb23ec08a07784957251d33808a23b3e3
lianet <jack.tolmak@yandex.ru>
2025-03-31 20:23:20 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -390,46 +390,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     if (document.querySelector('.related-cases__slider')) {
-        function toggleNavigation() {
-            const sliderElement = document.querySelector('.related-cases__slider');
-            if (!sliderElement || !sliderElement.swiper) return;
-
-            const slider = sliderElement.swiper;
-            const container = sliderElement;
-            const nextBtn = document.querySelector('.related-cases__next');
-            const prevBtn = document.querySelector('.related-cases__prev');
-
-            if (!container || !nextBtn || !prevBtn) return;
-
-            const sliderWidth = slider.virtualSize;
-
-            const containerWidth = container.clientWidth;
-            console.log(containerWidth);
-            const viewportWidth = window.innerWidth;
-
-
-            const marginRight = (viewportWidth - containerWidth) / 2;
-            const totalAvailableWidth = containerWidth + marginRight;
-
-            if (sliderWidth <= totalAvailableWidth) {
-                nextBtn.style.display = 'none';
-                prevBtn.style.display = 'none';
-            } else {
-                nextBtn.style.display = '';
-                prevBtn.style.display = '';
-            }
-        }
-
-        window.addEventListener("resize", () => {
-            toggleNavigation();
-        });
 
 
         new Swiper('.related-cases__slider', {
             slidesPerView: "auto",
             spaceBetween: 1,
-            observeParents: true,
-            watchSlidesProgress: true,
+
+            watchOverflow: true,
             navigation: {
                 nextEl: ".related-cases__next",
                 prevEl: ".related-cases__prev",
@@ -438,9 +405,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 el: ".related-cases__pagination",
                 clickable: true
             },
-            on: {
-                init: toggleNavigation,
-            }
+
         })
 
 
